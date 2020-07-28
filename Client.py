999c85eb9c8b38dd2edbd512284b0b9673ac7d59
Micraow <15190450708@163.com>
2020-07-28 09:12:18 +0800

    版权！

--- Client.py
+++ Client.py
@@ -1,23 +1,22 @@
 # coding=utf-8
-from O365 import Account
-import os
+
+#  O365-Client -- A client to connect O365
+#  Copyright (C) 2020  Micraow
+#
+#  This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from mailboxactions import *
-
-# O365-Client -- A client to connect O365
-# Copyright (C) 2020  Micraow
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 credentials = ('74424fcf-55d7-4e15-99d7-1663c0ba2e94',)
 account = Account(credentials, auth_flow_type='public')
@@ -26,7 +25,7 @@ account = Account(credentials, auth_flow_type='public')
 class Start:
     """这里是应用入口"""
 
-    def __init__(self,choice=None):
+    def __init__(self, choice=None):
         self.choice = choice
 
     def mail_or_calendar(self):
